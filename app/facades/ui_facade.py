d696015af5312312bc549acc5d7a1727070209a4
Joseph Hilby <joseph.hilby@gmail.com>
2025-03-13 13:59:20 -0500

    add pygame_gui

--- app/facades/ui_facade.py
+++ app/facades/ui_facade.py
@@ -1,15 +1,23 @@
 import sys
 
+from pygame_gui import UIManager
+
 from app.screens.menu import *
 
 class UIFacade:
-    def __init__(self, display: pygame.display):
+    def __init__(self, display: pygame.display, manager: UIManager):
+        self.state = "MENU"
         self.display: pygame.display = display
-        self.menu_screen: MenuScreen = MenuScreen()
+        self.manager: pygame_gui.UIManager = manager
+        self.menu_screen: MenuScreen = MenuScreen(self.manager)
 
-    def render(self, state: str):
-        if state == 'MENU':
-            self.menu_screen.render(self.display)
+    def render(self):
+        if self.state == 'MENU':
+            self.menu_screen.render()
+
+    def handle(self, event: pygame.event.Event):
+        if self.state == 'MENU':
+            self.menu_screen.handle(event)
 
     @staticmethod
     def quit():
--- lib/constants.py
+++ lib/constants.py
@@ -5,11 +5,11 @@ SCALER = 1
 SCREEN_WIDTH = (1000 * SCALER)
 SCREEN_HEIGHT = (500 * SCALER)
 
-BG_COLOR = (255, 255, 240)
+BG_COLOR = (211, 211, 211)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-DEFAULT_FONT = 'Arial'
+DEFAULT_FONT = 'Times New Roman'
 
 MENU_TXT = "Select your two algorithms to compare and click 'Start' below to begin. Click 'Exit' to close the program."
 MENU_FONT_SIZE = (20 * SCALER)
