0243e1f2b1f62a5e61deba39599466a637b5c5f6
Joseph Hilby <joseph.hilby@gmail.com>
2025-03-14 17:19:16 -0500

    add dropdown names from alg facade

--- app/algorithms/__init__.py
+++
@@ -1,7 +0,0 @@
-from flood_fill import FloodFill
-from wall_follow import WallFollow
-
-wf = WallFollow()
-ff = FloodFill()
-
-algorithms = [ff, wf]
--- app/screens/menu.py
+++ app/screens/menu.py
@@ -4,7 +4,7 @@ import pygame_gui
 
 from pygame_gui.elements import UIButton, UIDropDownMenu
 from lib.constants import *
-from app.algorithms import *
+from app.facades.algorithm_facade import AlgorithmFacade
 
 
 class MenuScreen:
@@ -14,27 +14,28 @@ class MenuScreen:
         self.start_button: UIButton | None = None
         self.alg_one_dropdown: UIDropDownMenu | None = None
         self.alg_two_dropdown: UIDropDownMenu | None = None
+        self.alg_names: [str] = AlgorithmFacade().names
 
     def render(self):
         if self.start_button is None:
             self.start_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((SCREEN_WIDTH * 0.72, SCREEN_HEIGHT * 0.86), (100, 50)),
-                                                         text='Start',
-                                                         manager=self.manager)
+                                                             text='Start',
+                                                             manager=self.manager)
         if self.exit_button is None:
             self.exit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((SCREEN_WIDTH * 0.85, SCREEN_HEIGHT * 0.86), (100, 50)),
-                                                         text='Quit',
-                                                         manager=self.manager)
+                                                            text='Quit',
+                                                            manager=self.manager)
 
         if self.alg_one_dropdown is None:
-            self.alg_one_dropdown = pygame_gui.elements.UIDropDownMenu(relative_rect=pygame.Rect(SCREEN_WIDTH * 0.55, SCREEN_HEIGHT * 0.5, 100, 50),
-                                                                       options_list=[algorithm.name for algorithm in algorithms],
-                                                                       starting_option=algorithms[0].name,
+            self.alg_one_dropdown = pygame_gui.elements.UIDropDownMenu(relative_rect=pygame.Rect(SCREEN_WIDTH * 0.55, SCREEN_HEIGHT * 0.5, 110, 25),
+                                                                       options_list=self.alg_names(),
+                                                                       starting_option=self.alg_names()[0],
                                                                        manager=self.manager)
 
         if self.alg_two_dropdown is None:
-            self.alg_two_dropdown = pygame_gui.elements.UIDropDownMenu(relative_rect=pygame.Rect(SCREEN_WIDTH * 0.35, SCREEN_HEIGHT * 0.5, 100, 50),
-                                                                       options_list=[algorithm.name for algorithm in algorithms],
-                                                                       starting_option=algorithms[0].name,
+            self.alg_two_dropdown = pygame_gui.elements.UIDropDownMenu(relative_rect=pygame.Rect(SCREEN_WIDTH * 0.35, SCREEN_HEIGHT * 0.5, 110, 25),
+                                                                       options_list=self.alg_names(),
+                                                                       starting_option=self.alg_names()[0],
                                                                        manager=self.manager)
 
     def handle(self, event: pygame.event.Event):
